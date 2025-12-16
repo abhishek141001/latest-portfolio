a9e175e10acf70af65f043ae0370ffff4aaafcf9
Abhishek Raj <105824150+abhishek141001@users.noreply.github.com>
2025-12-16 21:35:20 +0530

    Remove token data from Script tag in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
         <Providers>
           <div className="flex min-h-screen flex-col">
             <Navbar />
-<Script defer src="https://www.linked2web.com/embed.js" data-app-id="69404b831a8f8789767fa30a" data-api-url="https://linked2web-2-0-server-817687524421.asia-south2.run.app" data-token-endpoint="https://linked2web-2-0-server-817687524421.asia-south2.run.app/api/websites/69404b831a8f8789767fa30a/embed-token" data-token="emb_c59d1cf49bfc4300b09616a3a7a110346d8b9d36b455dd2f36d30140ded9baba" data-theme="light"></Script>
+<Script defer src="https://www.linked2web.com/embed.js" data-app-id="69404b831a8f8789767fa30a" data-api-url="https://linked2web-2-0-server-817687524421.asia-south2.run.app" data-token-endpoint="https://linked2web-2-0-server-817687524421.asia-south2.run.app/api/websites/69404b831a8f8789767fa30a/embed-token" data-theme="light"></Script>
             <main className="flex-1">{children}</main>
             <Footer />
           </div>
